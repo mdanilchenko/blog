1bfcc72825e02bb84b6b68ccba8cf8ae5c2f0e45
Maksim Danilchenko <ms.danilchenko@gmail.com>
2017-07-10 16:29:53 +0300

    fix post form

--- controllers/PostAddController.php
+++ controllers/PostAddController.php
@@ -5,8 +5,11 @@ class PostAddController extends Controller
     public function evalRequest(Request $request,Response $response){
         //Evaling incoming params
         $user = null;
-        if(isset($_SESSION['id'])){
+        if(isset($_SESSION['id']) and is_numeric($_SESSION['id'])){
             $user = DAOMethods::getUserById($_SESSION['id']);
+            if(!is_null($user)){
+                $response->addParam('user',$user);
+            }
         }
         if(is_null($user)){
             $this->goToController('HomeController', $request,$response);
